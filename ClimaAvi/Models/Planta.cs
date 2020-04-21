faeac43e1dfe8081a4ae45ea64ccfff7f6d3c724
Emachado377 <evandromachado@acad.ftec.com.br>
2020-04-20 21:31:07 -0300

    Models
    Adicionado:
    1- Layout Autenticação
    2-Models - Usuario, Planta

---
+++ ClimaAvi/Models/Planta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClimaAvi.Models
+{
+    public class Planta
+    {
+        public int CodigoPlanta { get; set; }
+        public string NomePlanta { get; set; }
+        public double Temperatura { get; set; }
+        public DateTime LeituraTemperatura { get; set; }
+        public double UmidadeAr { get; set; }
+        public DateTime LeituraUmidadeAr { get; set; }
+        public double Gas { get; set; }
+        public DateTime LeituraGas { get; set; }
+
+        public Planta()
+        {
+        }
+    }
+}
---
+++ ClimaAvi/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿namespace ClimaAvi.Models
+{
+    public class Usuario
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Telefone { get; set; }
+
+        public Usuario()
+        {
+        }
+
+        public Usuario(int codigo, string nome, string email, int telefone)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Email = email;
+            Telefone = telefone;
+        }
+    }
+}
