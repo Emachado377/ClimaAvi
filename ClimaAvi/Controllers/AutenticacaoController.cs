4197dd20979ab7a1caee8497421031f6e7e84d58
Emachado377 <evandromachado@acad.ftec.com.br>
2020-04-23 22:40:44 -0300

    Editado Controllers Autenticação
    1- Login
    2- Validação

--- ClimaAvi/Controllers/AutenticacaoController.cs
+++ ClimaAvi/Controllers/AutenticacaoController.cs
@@ -14,17 +14,28 @@ namespace ClimaAvi.Controllers
         {
 
 
-            return View();
+            return RedirectToAction("Login");
         }
 
         public ActionResult Login()
         {
-          
-           return View();
+            List<User> users; // declarando a lista
+            users = (List<User>)Session["users"]; // atribuindo a sessão com um casting forçado
+
+            ViewBag.users = users;
+
+
+            return View();
         }
       
-        public ActionResult Register()
+        public ActionResult Register(User user)
         {
+            List<User> users; // declarando a lista
+            users = (List<User>)Session["users"]; // atribuindo a sessão com um casting forçado
+
+            users.Add(user); // Adicionamos os dados informados do novo usuario na lista users
+
+            Session["users"] = users;  // atribuimos a lista atualizada de usuários para a sessão, Global.asax
 
             return View();
         }
@@ -32,5 +43,28 @@ namespace ClimaAvi.Controllers
         {
             return View();
         }
+
+        public ActionResult Validation(User user)
+        {
+            List<User> users; // declarando a lista
+            users = (List<User>)Session["users"]; // atribuindo a sessão com um casting forçado
+
+           
+            foreach ( var name in users)
+            {
+                if ( name.Name == user.Name)
+                {
+                    if ( name.Password == user.Password)
+                    {
+                        return RedirectToAction("Index", "Home"); ;
+                    }
+                }
+            }
+          return  RedirectToAction("Index"); ;
+            
+            
+        }
+
+
     }
 }
